e922c9fa009e6cbc6e19ed1b995a55393fbceab7
AnGlonchas2 <jose.preciado2781@alumnos.udg.mx>
2025-03-07 09:20:52 -0600

    Add files via upload

--- cajeroSim2/main.c
+++ cajeroSim2/main.c
@@ -1,1 +1,27 @@
+#include "include/ATM.h"
 
+int main(){
+    int usernum = 0;
+    int mode = 0;
+    int run = 1;
+
+    User* userarr = malloc(usernum*sizeof(User)*(usernum+1));
+
+    while(run){
+        switch (mode){
+            case 0: mMenu(&mode); break;
+            case 1: 
+                newUser(userarr, &usernum, &mode);
+                usernum++;
+                break;
+            case 2: retire(userarr, &usernum, &mode); break;
+            case 3: deposit(userarr, &usernum, &mode); break;
+            case 4: getInfo(userarr, &usernum, &mode); break;
+            case 5: selectUser(userarr, &usernum, &mode); break;
+            case 6: run = 0; break;
+            default: mMenu(&mode);
+        }
+    }
+    free(userarr);
+    return 0;
+}
